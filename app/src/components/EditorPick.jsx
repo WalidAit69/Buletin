481e1a2b1c17016003a505b567ff566ff53f88dd
WalidAit69 <aitwalid2000@gmail.com>
2024-03-05 23:59:28 +0100

    update

--- app/src/components/EditorPick.jsx
+++ app/src/components/EditorPick.jsx
@@ -36,58 +36,46 @@ function MustRead({ unsortedposts }) {
       </div>
 
       <div className="Editorpick_posts">
-        <div className="Editorpick_cover">
-          <Image
-            className="Editorpick_cover_img"
-            src={randompost.cover}
-            alt={randompost.title}
-          />
-          <Link to={`post/${randompost._id}`}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
-              />
-            </svg>
-          </Link>
+        <Link to={`post/${randompost._id}`}>
 
-          <div className="Editorpick_info">
+          <div className="Editorpick_cover">
+            <Image
+              className="Editorpick_cover_img"
+              src={randompost.cover}
+              alt={randompost.title}
+            />
 
-            <div>
-              <Link to={`/user/${randompost?.author?._id}`}>
-                {profile && <Image className="Editorpick_info_img" src={profile} />}
-                {randompost?.author?.fullname}</Link> -
-              {
-                <ReactTimeAgo
-                  date={timestamp ? timestamp : "2023-08-22T10:12:34.922Z"}
-                  locale="en-US"
-                  timeStyle="long"
-                  className="custom-timeago"
-                />
-              }
-            </div>
-            <h1>{randompost.title}</h1>
-            <p>{truncatesummary}</p>
-            <div>
-              {randompost.topic} - {randompost.read} read
+            <div className="Editorpick_info">
+
+              <div>
+                <Link to={`/user/${randompost?.author?._id}`}>
+                  {profile && <Image className="Editorpick_info_img" src={profile} />}
+                  {randompost?.author?.fullname}</Link> -
+                {
+                  <ReactTimeAgo
+                    date={timestamp ? timestamp : "2023-08-22T10:12:34.922Z"}
+                    locale="en-US"
+                    timeStyle="long"
+                    className="custom-timeago"
+                  />
+                }
+              </div>
+              <h1>{randompost.title}</h1>
+              <p>{truncatesummary}</p>
+              <div>
+                {randompost.topic} - {randompost.read} read
+              </div>
             </div>
           </div>
-        </div>
+        </Link>
+
 
         <div className="Editorpick_Allposts">
           {unsortedposts.length > 0 && unsortedposts.slice(0, 4).map((post) => <Cards key={post._id} {...post}></Cards>)}
         </div>
 
       </div>
-    </div>
+    </div >
   );
 }
 
